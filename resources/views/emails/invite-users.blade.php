15219c008d32a4420f3611ae974794125d96e5a3
Sherriff Agboola <rylxes@gmail.com>
2021-06-02 22:27:11 +0100

    others

--- resources/views/emails/invite-users.blade.php
+++ resources/views/emails/invite-users.blade.php
@@ -1,11 +1,7 @@
 @component('mail::message')
     Hello, You have an invitation from {{$company->name}}, to join their team
 
-    @component('mail::button', ['url' => url('dashboard')])
-        Click Here to Continue
-    @endcomponent
-
-    URL : {{$url}}
+    Click on this URL to Continue : {{$url}}
 
     Thanks,
     {{ config('app.name') }}
